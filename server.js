a8257961b531f5fb35029a7e3aa3a4f741405bbc
Per Lundholm <per@artcomputer.se>
2021-11-19 16:24:01 +0100

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const {Server} = require("socket.io");
 
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ const indexHtml = '/index.html';
 
 const app = express()
     .use((req, res) => res.sendFile(indexHtml, { root: __dirname }))
+    .use(cors({origin: '*'}))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = new Server(app);
