354f0d15b168db5bb0e7fb9929a807120878d233
Per Lundholm <per@artcomputer.se>
2022-01-16 17:24:38 +0100

    Use namespaces to keep clients apart.

--- server.js
+++ server.js
@@ -14,17 +14,22 @@ const app = express()
 
 const io = new Server(app, {cors: {origin: '*'}});
 
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.broadcast.emit('signal', 'client connected');
+const workspaces = io.of(/^\/[0-9]{7}$/);
+
+workspaces.on('connection', (socket) => {
+    const workspace = socket.nsp;
+    console.log('Client connected to workspace ', workspace.name);
+    socket.broadcast.emit('signal', 'client connected ' + socket.id);
     socket.on('signal', function (data) {
-        console.log('Broadcasting signal', data);
+        console.log('Broadcasting signal "', data, '" in workspace', workspace.name);
         socket.broadcast.emit('signal', data);  // Echo to all but the sender.
     });
     socket.on('disconnect', () => {
         console.log('Client disconnected');
         socket.broadcast.emit('signal', 'client disconnected')
     });
+
+    setInterval(() => socket.emit('time', new Date().toTimeString()), 1000);
+
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
